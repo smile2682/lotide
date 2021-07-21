5d0d05290d5c9c2bfc7aac1e1ea86f79d352375c
smile2682 <smilecrystal.liyi@gmail.com>
2021-07-21 03:47:17 +0000

    2 for loops for boolean value

--- assertEqual.js
+++ assertEqual.js
@@ -1,4 +1,4 @@
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`😀😀Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -6,8 +6,30 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(10, 71);
-assertEqual("Bootcamp", "Bootcamp");
+
+const eqArrays = function (arr1, arr2) {
+  let result = []
+  let final = ''
+  for (let i = 0; i < arr1.length; i++) {
+
+    if (arr1[i] === arr2[i]) {
+      result.push(true)
+    } else { result.push(false) }
+
+  }
+  // console.log(result);
+  for (let i = 1; i < result.length; i++) {
+    if (result[i] === result[i - 1]) {
+      final = true
+    } else { final = false }
+  }
+  return final
+  console.log(final)
+}
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+// test code
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
