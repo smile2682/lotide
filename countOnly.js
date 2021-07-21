92cf8ae7afb83bc812c6230ba62627e145d26273
smile2682 <smilecrystal.liyi@gmail.com>
2021-07-21 21:33:39 +0000

    adding if statements to create the false-undefined correlaton.

---
+++ countOnly.js
@@ -0,0 +1,39 @@
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`😀😀Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😅😅 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const countOnly = function(allItems, itemsToCount) {const results = {}
+for (const item of allItems) {
+  // console.log(results);
+  if (results[item]) {
+    results[item] += 1;
+  } else {
+    results[item] = 1;
+  }
+  if(itemsToCount[item]===false){
+    results[item]= undefined
+  }
+}return results;
+}
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
