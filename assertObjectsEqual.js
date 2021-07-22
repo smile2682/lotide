f383f5f85d952bce22d6d959060d6a5ede4c49d9
smile2682 <smilecrystal.liyi@gmail.com>
2021-07-22 22:48:06 +0000

    needs assertion!

---
+++ assertObjectsEqual.js
@@ -0,0 +1,47 @@
+const eqObjects = function (object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  } else {
+    const keysObj1 = Object.keys(object1);
+    for (let key of keysObj1) {
+      if (Array.isArray(object1[key])) {
+        if (!eqArrays(object1[key], object2[key])) {
+          return false;
+        }
+      } else if (object1[key] !== object2[key]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+function eqArrays(arrayA, arrayB) {
+  if (arrayA.length !== arrayB.length) {
+    return false;
+  }
+  for (let i = 0; i < arrayA.length; i++) {
+    if (arrayA[i] !== arrayB[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+const assertObjectsEqual = function (actual, expected) {
+  // Implement me!
+  const inspect = require("util").inspect;
+  //console.log(`Example label: ${inspect(actual)}`);
+  if (eqObjects(actual, expected)) {
+    console.log(
+      `:+1: :+1: :+1: Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`
+    );
+  } else {
+    console.log(
+      `:-1: :-1: :-1: Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`
+    );
+  }
+};
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+const abc = { a: "1", b: "2", c: "3" };
+assertObjectsEqual(ab, ba);
+assertObjectsEqual(ab, abc);
---
+++ map.js
@@ -0,0 +1,17 @@
+const words = ["ground", "control", "to", "major", "tom"];
+const map = function(array,callback) {
+  // console.log('array: ', array);
+  // console.log('callback: ', callback);
+  const results = [];
+  for (let item of array) {
+    // console.log('item BEFORE: ', item);
+    // console.log('item AFTER: ', callback(item));
+    // console.log('---');
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+// console.log(map);
