707eb7800f8a52b5f21150feaafbec92c14a56ff
smile2682 <smilecrystal.liyi@gmail.com>
2021-07-23 04:50:26 +0000

    needs assertion

---
+++ findKey.js
@@ -0,0 +1,22 @@
+const findKey = function (obj,callback){
+  for (key in obj){
+      if(callback(obj[key])){
+       
+        return key;
+      }
+      
+    }
+  }
+
+
+
+const test = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+console.log (test)
