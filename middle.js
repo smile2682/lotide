f148613946f93275089062096ea80ad32f0f8be7
smile2682 <smilecrystal.liyi@gmail.com>
2021-07-26 21:47:27 +0000

    implement obj method again, feel like it is cleaner to require file from the origin instead of from where the origin is imported

--- middle.js
+++ middle.js
@@ -1,20 +1,4 @@
-const eqArrays = function (arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-
-    if (arr1[i] !== arr2[i]) {
-      return false
-    } 
-
-  }return true;
-}
-const assertArraysEqual = function (actual, expected) {
-  if (eqArrays(actual,expected)) 
-  {
-    console.log(`😀😀Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`😅😅 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertArraysEqual = require ('./assertArraysEqual').assertArraysEqual
 
 const middle = function (arr) {
   let newArr = [];
@@ -26,24 +10,5 @@ const middle = function (arr) {
     newArr.push(arr[(arr.length) / 2 - 1], arr[(arr.length) / 2])
   } return newArr;
 }
-// }
 
-// testcode
-
-// For arrays with one or two elements, there is no middle. Return an empty array.
-
-// console.log(middle([1])) // => []
-// console.log(middle([1, 2])) // => []
-
-// // For arrays with odd number of elements, an array containing a single middle element should be returned.
-
-// console.log(middle([1, 2, 3]))// => [2]
-// console.log(middle([1, 2, 3, 4, 5])) // => [3]
-
-// For arrays with an even number of elements, an array containing the two elements in the middle should be returned
-
-console.log(middle([1, 2, 3, 4])) // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6]))// => [3, 4]
-
-assertArraysEqual(middle([1, 2, 3, 4]) , [2, 3])
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])
+module.exports = {middle, assertArraysEqual};
