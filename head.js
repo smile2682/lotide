a4a1c626f04e9b772e7019ba44933d9abe142017
smile2682 <smilecrystal.liyi@gmail.com>
2021-07-26 20:20:44 +0000

    implemented test to seperate function files and test files. So much cleanergit add .

--- head.js
+++ head.js
@@ -1,15 +1,7 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`😀😀Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`😅😅 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// const assertEqual = require('./assertEqual');
+
 const head = function (arr) {
   return arr[0];
 };
-// TEST CODE
-assertEqual(head([5]), 5);
-assertEqual(head([]));
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+module.exports = head
